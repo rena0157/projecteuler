24a2af6ac67c77e19dfc4c9765058c3ef0cf0967
Adam Renaud <adam.renaud@abicbuilds.com>
2021-09-18 11:15:23 -0400

    adding problem 4

---
+++ src/Problem4/Program.cs
@@ -0,0 +1,43 @@
+﻿// Project Euler - Problem 4
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+string ReverseString(string input)
+{
+    return string.Create(input.Length, input, (chars, state) =>
+    {
+        var position = 0;
+        for (var index = state.Length - 1; index >= 0; index--)
+        {
+            chars[position++] = state[index];
+        }
+    });
+}
+
+bool IsPalindromic(long number)
+{
+    var numberString = number.ToString();
+    var reversedString = ReverseString(numberString);
+    return reversedString == numberString;
+}
+
+IEnumerable<(int Left, int Right)> GetProducts(int start, int end)
+{
+    for (var left = start; left < end; left++)
+    {
+        for (var right = left; right < end; right++)
+        {
+            yield return (left, right);
+        }
+    }
+}
+
+var palindromicNumbers = GetProducts(100, 1000)
+    .Select(f => f.Left * f.Right)
+    .Where(n => IsPalindromic(n))
+    .ToList();
+
+var max = palindromicNumbers.Max();
+Console.WriteLine($"Max: {max}");
