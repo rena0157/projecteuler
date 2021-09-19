9461a49b1bfb99f8ef2aa77c6692805bf4f3b556
Adam Renaud <adam.renaud@abicbuilds.com>
2021-09-19 15:22:39 -0400

    adding problem 6

---
+++ src/Problem6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Project Euler - Problem 6
+using System;
+using System.Linq;
+
+var sumOfSquares = Enumerable.Range(1, 100).Select(n => (int)Math.Pow(n, 2.0)).Sum();
+var squareOfSum = (int)Math.Pow(Enumerable.Range(1, 100).Sum(), 2);
+var answer = squareOfSum - sumOfSquares;
+Console.WriteLine($"Answer: {answer}");
