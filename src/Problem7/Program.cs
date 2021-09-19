8f8b635ad3407fc5f003800cfb46c00a8c885870
Adam Renaud <adam.renaud@abicbuilds.com>
2021-09-19 16:26:34 -0400

    adding problem 7

---
+++ src/Problem7/Program.cs
@@ -0,0 +1,41 @@
+﻿// Project Euler - Problem 7
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+bool IsPrime(int number)
+{
+    switch (number)
+    {
+        case < 2:
+            return false;
+        case 2:
+            return true;
+    }
+
+    if (number % 2 == 0)
+        return false;  
+      
+    for (var factor = 3; factor <= (int)Math.Sqrt(number); factor++)  
+    {  
+        if (number % factor == 0)  
+            return false;  
+    }  
+  
+    return true;  
+}
+
+IEnumerable<int> GetNumbers(int max)  
+{  
+    var current = 0;  
+    while (current < max)  
+        yield return current++;  
+}
+
+
+var answer = GetNumbers(int.MaxValue)
+     .Where(IsPrime)
+     .ElementAt(10_000);
+
+Console.WriteLine(answer);
