f70f70949f87f70a18f4ee7281052ec2b03d593c
Adam Renaud <adam.renaud@abicbuilds.com>
2021-09-19 19:37:36 -0400

    adding problem 9

---
+++ src/Problem9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+(int, int, int) GetFactors()
+{
+    for (var a = 1; a < 1000; a++)
+    {
+        for (var b = a + 1; b < 1000; b++)
+        {
+            for (var c = b + 1;c < 1000; c++)
+            {
+                var sum = a + b + c;
+
+                if (sum == 1000 && a * a + b * b == c * c)
+                    return (a, b, c);
+                
+                if (sum > 1000)
+                    break;
+            }
+        }
+    }
+
+    throw new Exception("No Solution Found");
+}
+
+var factors = GetFactors();
+var product = factors.Item1 * factors.Item2 * factors.Item3;
+
+Console.WriteLine($"Project Euler - Problem 9 Solution: {product}");
