e4d05cd24feddba194bbd32c76d78df98ba93e74
Adam Renaud <adam.renaud@abicbuilds.com>
2021-09-19 20:02:33 -0400

    adding problem 10

--- src/P0010/Program.cs
+++ src/P0010/Program.cs
@@ -1,3 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+IEnumerable<long> GetNumbers(long max)    
+{    
+    var current = 0;    
+    while (current < max)    
+        yield return current++;    
+}
+
+bool IsPrime(long number)  
+{  
+    switch (number)  
+    {  
+        case < 2:  
+            return false;  
+        case 2:  
+            return true;  
+    }  
+  
+    if (number % 2 == 0)  
+        return false;    
+        
+    for (var factor = 3; factor <= (long)Math.Sqrt(number); factor++)    
+    {    
+        if (number % factor == 0)    
+            return false;    
+    }    
+    
+    return true;    
+}
+
+long Solution(long maxPrime)
+{
+    var sum = 0L;
+    
+    foreach (var number in GetNumbers(long.MaxValue))
+    {
+        if (!IsPrime(number))
+            continue;
+
+        if (number >= maxPrime)
+            return sum;
+        
+        sum += number;
+    }
+
+    throw new Exception("Sum not reached");
+}
+
+var solution = Solution(2_000_000);
+Console.WriteLine(solution);
